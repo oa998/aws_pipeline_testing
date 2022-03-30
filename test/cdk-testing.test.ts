7d6381ba621dc2a7a75bda59fdd6f31ac8adc3fd
zzz sss <onlineaccounts998@gmail.com>
2022-03-30 12:13:41 -0400

    auto commit

--- test/cdk-testing.test.ts
+++ test/cdk-testing.test.ts
@@ -3,5 +3,5 @@ import { handler } from "../lib/lambda/hello-world";
 //https://docs.aws.amazon.com/codebuild/latest/userguide/test-reporting.html
 test('should fail', async () => {
   const result = await handler("", "");
-  expect(result.statusCode).toEqual(300);
+  expect(result.statusCode).toEqual(200);
 });

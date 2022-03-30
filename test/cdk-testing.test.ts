c88f7a8632df9a2a0f5433d4cba34de1b19e59a1
zzz sss <onlineaccounts998@gmail.com>
2022-03-30 10:54:31 -0400

    first

---
+++ test/cdk-testing.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkTesting from '../lib/cdk-testing-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-testing-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkTesting.CdkTestingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
